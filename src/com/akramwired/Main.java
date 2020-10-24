b2c54b5959e68bec7f4e0122213bb9b0d5c8024b
Akramwired <kahd.unplugged@gmail.com>
2020-10-24 21:11:46 +0600

    integer overflow and underflow

---
+++ src/com/akramwired/Main.java
@@ -0,0 +1,17 @@
+package com.akramwired;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myValue = 10000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted Maximum Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Minimum Value = " + (myMinIntValue - 1));
+        int myMaxValue = 2_147_483_647;
+        System.out.println(myMaxIntValue);
+    }
+}
