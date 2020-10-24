d28741a5aadc6eb8c627eda357bfff3aead468c6
Akramwired <kahd.unplugged@gmail.com>
2020-10-24 21:41:43 +0600

    testing byte long short

--- src/com/akramwired/Main.java
+++ src/com/akramwired/Main.java
@@ -11,7 +11,27 @@ public class Main {
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
         System.out.println("Busted Maximum Value = " + (myMaxIntValue + 1));
         System.out.println("Busted Minimum Value = " + (myMinIntValue - 1));
+
         int myMaxValue = 2_147_483_647;
         System.out.println(myMaxIntValue);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 1000L;
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
     }
 }
